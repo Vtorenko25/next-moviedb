1c8f9cab296d4fb6c495afe8250ebd868e79f354
Vtorenko25 <oleg.vtorenko@gmail.com>
2024-11-24 22:49:58 +0200

    comit

--- src/app/(pages)/movies/[id]/layout.tsx
+++ src/app/(pages)/movies/[id]/layout.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Metadata} from "next";
+import {movieService} from "@/components/services/api.service";
 
-export const metadata: Metadata = {
-    title: 'MovieLayout metadata'
+export const generateMetadata = async ({params}:{params:{id:string}}):Promise<Metadata> =>{
+    console.log(params.id, '111');
+    let movie = await movieService.getMovie(params.id)
+    return {title: movie.title, description: movie.description}
 }
 type Props = { children: React.ReactNode }
 const MovieLayout = ({children}: Props) => {
--- src/app/(pages)/movies/layout.tsx
+++ src/app/(pages)/movies/layout.tsx
@@ -4,6 +4,7 @@ import {Metadata} from "next";
 export const metadata: Metadata = {
     title: 'MoviesLayout metadata'
 }
+
 type Props = { children: React.ReactNode }
 const MoviesLayout = ({children}: Props) => {
     return (
